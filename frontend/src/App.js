03bd4e1fb39782d1966e2c182a4a12afadb6db18
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-06-25 21:11:45 +0530

    Add Router Configuration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,29 @@ import React from "react";
 import "./App.css";
 import Navbar from "./component/Navbar/Navbar";
 import Home from "./page/Home/Home";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navbar />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
   return (
     <>
-      <Navbar />
-      <Home />
+      {/* Passing all data router objects to this component to render the app and enable the rest of the data APIs */}
+      <RouterProvider router={router} />
     </>
   );
 }
--- frontend/src/component/Navbar/Navbar.js
+++ frontend/src/component/Navbar/Navbar.js
@@ -1,30 +1,34 @@
 import styles from "./Navbar.module.css";
 import logo from "../../assets/images/logo.png";
+import { Outlet } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className={styles.navbarContainer}>
-      {/* <!--Logo Container--!> */}
-      <div className={styles.logoContainer}>
-        <img src={logo} alt="Logo" className={styles.logo} />
-        <div className={styles.logoName}>
-          Movie <span className={styles.nameRight}>Mania</span>
+    <>
+      <div className={styles.navbarContainer}>
+        {/* <!--Logo Container--!> */}
+        <div className={styles.logoContainer}>
+          <img src={logo} alt="Logo" className={styles.logo} />
+          <div className={styles.logoName}>
+            Movie <span className={styles.nameRight}>Mania</span>
+          </div>
+        </div>
+        {/* <!--Nav Item Container--!> */}
+        <div className={styles.navItemContainer}>
+          <div className={styles.navList}>
+            <div>Home</div>
+            <div>Watch List</div>
+            <div>About</div>
+          </div>
+        </div>
+        {/* <!--User Container--!> */}
+        <div className={styles.userSection}>
+          <div className={styles.userName}>Hi! Sara</div>
+          <div className={styles.addToWatchList}>SR</div>
         </div>
       </div>
-      {/* <!--Nav Item Container--!> */}
-      <div className={styles.navItemContainer}>
-        <div className={styles.navList}>
-          <div>Home</div>
-          <div>Watch List</div>
-          <div>About</div>
-        </div>
-      </div>
-      {/* <!--User Container--!> */}
-      <div className={styles.userSection}>
-        <div className={styles.userName}>Hi! Sara</div>
-        <div className={styles.addToWatchList}>SR</div>
-      </div>
-    </div>
+      <Outlet />
+    </>
   );
 }
 
