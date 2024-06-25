94bb06fbee34c0bf6829e5fbc3c383e51248a8ad
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-06-25 17:55:16 +0530

    Add Navbar

---
+++ frontend/src/component/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import styles from "./Navbar.module.css";
+import logo from "../../assets/images/logo.png";
+
+function Navbar() {
+  return (
+    <div className={styles.navbarContainer}>
+      {/* <!--Logo Container--!> */}
+      <div className={styles.logoContainer}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+        <div className={styles.logoName}>
+          Movie <span className={styles.nameRight}>Mania</span>
+        </div>
+      </div>
+      {/* <!--Nav Item Container--!> */}
+      <div className={styles.navItemContainer}>
+        <div className={styles.navList}>
+          <div>Home</div>
+          <div>Watch List</div>
+          <div>About</div>
+        </div>
+      </div>
+      <div className={styles.userSection}>
+        <div className={styles.userName}>Hi! Sara</div>
+        <div className={styles.addToWatchList}>SR</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
